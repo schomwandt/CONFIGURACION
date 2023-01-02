1575d887d9ee8b8517a0c189eda27cffe6328a97
davschom <schomwandt@gmail.com>
2023-01-02 15:24:33 -0300

    configuracion

---
+++ configuracion.R
@@ -0,0 +1,19 @@
+
+# Git y GitHub con R
+
+# conexión y configuración 
+
+http://destio.us.es/calvo/asignaturas/ge_esco/tutorialusargitgithubrstudio/UsarGitGithubconRStudio.html#:~:text=La%20forma%20m%C3%A1s%20sencilla%20de,de%20su%20repositorio%20y%20listo.
+
+
+install.packages("devtools")
+
+library(usethis)
+
+edit_git_config()
+
+###############################
+
+library(usethis)
+
+library(gitcreds)
