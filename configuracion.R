bad3338d28197f5417d545c4ed08d540774738c7
DAVID SCHOMWANDT <64860973+schomwandt@users.noreply.github.com>
2023-01-02 19:39:49 -0300

    Update configuracion.R

--- configuracion.R
+++ configuracion.R
@@ -2,8 +2,9 @@
 # Git y GitHub con R
 
 # conexión y configuración 
+# Configuración para acceso al repositorio de github:
 
-http://destio.us.es/calvo/asignaturas/ge_esco/tutorialusargitgithubrstudio/UsarGitGithubconRStudio.html#:~:text=La%20forma%20m%C3%A1s%20sencilla%20de,de%20su%20repositorio%20y%20listo.
+# http://destio.us.es/calvo/asignaturas/ge_esco/tutorialusargitgithubrstudio/UsarGitGithubconRStudio.html#:~:text=La%20forma%20m%C3%A1s%20sencilla%20de,de%20su%20repositorio%20y%20listo.
 
 
 install.packages("devtools")
